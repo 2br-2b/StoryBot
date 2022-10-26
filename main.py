94336092cbdb7fadecab5c17a9c3c5283925b8a0
John Wuller <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2022-10-25 20:10:34 -0400

    Added error checking for the config file

--- main.py
+++ main.py
@@ -2,13 +2,33 @@ import pkg_resources
 pkg_resources.require("discord.py==1.7.3")
 
 import discord
-import config
+
+
+try:
+    import config
+except ImportError:
+    # Check if the file exists
+    # If it doesn't, copy the example file
+    from os.path import exists
+    if not exists("config.py"):
+        import shutil
+        shutil.copyfile("config.py.default", "config.py")
+        print("Please edit your config file (config.py) and then restart the bot.")
+        exit()
+    else:
+        print("Something went wrong importing the config file. Check your config file for any errors.")
+        exit()
+
+
 import dmlistener
 from user_manager import user_manager
 from file_manager import file_manager
 from discord.ext import commands
 from threading import Thread
 import time
+
+
+
 
 
 bot = commands.Bot("s.", help_command = None)
