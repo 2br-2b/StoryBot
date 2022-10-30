b870e40a32fe7613a27f6bc0bc4ee2f8db9abd8e
John Wuller <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2022-10-30 17:18:23 -0400

    Removed redundant code

--- file_manager.py
+++ file_manager.py
@@ -5,27 +5,24 @@ import config
 class file_manager():
     def __init__(self):
         file = open("story.txt", "r", encoding="utf8")
-        self.story = file.read()
         file.close()
         
     # Returns the story in the story.txt file
     def getStory(self):
-        file = open("story.txt", "r", encoding="utf8")
-        self.story = file.read()
-        file.close()
-        return self.story
+        with open("story.txt", "r", encoding="utf8") as file:
+            return file.read()
 
     # Appends the given line to the story and writes it to the file
     def addLine(self, line):
+        line = file_manager.fix_line_ending(line).replace("\\","\n")
+
+        # Makes sure the user isn't sending a command before writing the story
+        # Since this is already checked in dmlistener, this throws an error when it starts with a command
+        if line.startswith(config.PREFIX):
+            raise RuntimeWarning("I was just told to add this to the story, but this is clearly a command:"+line)
+        
         with open("story.txt", "a", encoding="utf8") as append_to:
-            line = file_manager.fix_line_ending(line).replace("\\","\n")
-
-            if not line.startswith(config.PREFIX):
-                self.story += line
             append_to.write(line)
-
-        with open("story.txt", "r", encodig="utf8") as f:
-            self.story = f.read()
 
     # A work in progress
     # Should save the old story and restart the current story from scratch
